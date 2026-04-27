78ac7f45ccd06e302aa6fa0b32f3cec4b19b878e
vernoxy <vernoxy3@gmail.com>
2026-04-27 15:17:52 +0530

    fix: erp system

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,20 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth"; // ← Add this
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDCRu5eU22MoG1uJi56n5wnfnVZhTaR5WI",
-  authDomain: "fib2fab-3d3e0.firebaseapp.com",
-  projectId: "fib2fab-3d3e0",
-  storageBucket: "fib2fab-3d3e0.firebasestorage.app",
-  messagingSenderId: "103833128120",
-  appId: "1:103833128120:web:5edfcd10038d0e4b19f84d",
-  measurementId: "G-9Z0LREK3N0"
+  apiKey: "AIzaSyCX5nYBz-HcNasQ9mlD3oly1teBWPOUm7c",
+  authDomain: "erp-system-bd67c.firebaseapp.com",
+  projectId: "erp-system-bd67c",
+  storageBucket: "erp-system-bd67c.firebasestorage.app",
+  messagingSenderId: "745506561823",
+  appId: "1:745506561823:web:31db9b15f30a5251fb9a2a",
+  measurementId: "G-L1MZ8HDDYN"
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const auth = getAuth(app); // ← Add this
-export default app;
